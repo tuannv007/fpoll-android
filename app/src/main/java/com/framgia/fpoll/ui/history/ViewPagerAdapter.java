9d6e469385b7c79b8c5023f15098e378203d23e4
tuannv007 <tuan.dev.java@gmail.com>
2017-04-10 16:25:49 +0700

    fix codinate layout vote screen

--- app/src/main/java/com/framgia/fpoll/ui/history/ViewPagerAdapter.java
+++ app/src/main/java/com/framgia/fpoll/ui/history/ViewPagerAdapter.java
@@ -3,7 +3,6 @@ package com.framgia.fpoll.ui.history;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,6 +13,7 @@ import java.util.List;
 public class ViewPagerAdapter extends FragmentPagerAdapter {
     private List<Fragment> mListFragment = new ArrayList<>();
     private String[] mTitle;
+    public static final int OFFSET_VIEWPAGER = 3;
 
     public ViewPagerAdapter(FragmentManager fm, List<Fragment> fragments, String[] titles) {
         super(fm);
